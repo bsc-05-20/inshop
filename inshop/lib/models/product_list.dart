90b5cfe387a259d19e6215000ef8b8bd4ee31c55
Morten Msiska <bsc-05-20@unima.ac.mw>
2024-11-04 14:42:25 -0800

    Increased product list

--- inshop/lib/models/product_list.dart
+++ inshop/lib/models/product_list.dart
@@ -10,5 +10,23 @@ final List<ProductModel> products = [
   ProductModel(imageUrl: 'assets/laptop.jpg', title: 'Laptop', rating: 5.1, price: 500000, category: 'Accessories'),
   ProductModel(imageUrl: 'assets/House2.jpg', title: 'House 2', rating: 5.1, price: 500000, category: 'Furniture'),
   ProductModel(imageUrl: 'assets/Iphone.jpg', title: 'Iphone', rating: 5.1, price: 500000, category: 'Electronics'),
+  //
+  ProductModel(imageUrl: 'assets/House2.jpg', title: 'House 2', rating: 5.1, price: 500000, category: 'Estate'),
+  ProductModel(imageUrl: 'assets/Ball.jpg', title: 'Ball', rating: 5.1, price: 500000, category: 'Sports'),
+  ProductModel(imageUrl: 'assets/Pant1.jpg', title: 'Pants', rating: 5.1, price: 500000, category: 'Clothes'),
+  ProductModel(imageUrl: 'assets/Shoe.jpg', title: 'Shoe', rating: 5.1, price: 500000, category: 'Shoes'),
+  ProductModel(imageUrl: 'assets/Car.jpg', title: 'Car', rating: 5.1, price: 500000, category: 'Cars'),
+  ProductModel(imageUrl: 'assets/laptop.jpg', title: 'Laptop', rating: 5.1, price: 500000, category: 'Accessories'),
+  ProductModel(imageUrl: 'assets/House2.jpg', title: 'House 2', rating: 5.1, price: 500000, category: 'Furniture'),
+  ProductModel(imageUrl: 'assets/Iphone.jpg', title: 'Iphone', rating: 5.1, price: 500000, category: 'Electronics'),
+  //
+  ProductModel(imageUrl: 'assets/House2.jpg', title: 'House 2', rating: 5.1, price: 500000, category: 'Estate'),
+  ProductModel(imageUrl: 'assets/Ball.jpg', title: 'Ball', rating: 5.1, price: 500000, category: 'Sports'),
+  ProductModel(imageUrl: 'assets/Pant1.jpg', title: 'Pants', rating: 5.1, price: 500000, category: 'Clothes'),
+  ProductModel(imageUrl: 'assets/Shoe.jpg', title: 'Shoe', rating: 5.1, price: 500000, category: 'Shoes'),
+  ProductModel(imageUrl: 'assets/Car.jpg', title: 'Car', rating: 5.1, price: 500000, category: 'Cars'),
+  ProductModel(imageUrl: 'assets/laptop.jpg', title: 'Laptop', rating: 5.1, price: 500000, category: 'Accessories'),
+  ProductModel(imageUrl: 'assets/House2.jpg', title: 'House 2', rating: 5.1, price: 500000, category: 'Furniture'),
+  ProductModel(imageUrl: 'assets/Iphone.jpg', title: 'Iphone', rating: 5.1, price: 500000, category: 'Electronics'),
 ];
 
